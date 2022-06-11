65ec599bdc968392609e89ecc72ab74b4f0da263
Renz Ivan Irag <66311197+Kartoflur@users.noreply.github.com>
2022-06-12 07:50:56 +0800

    Add files via upload

---
+++ static/css/precache-manifest.bd1892b9b9de3643ef9358785d104b06.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c4eefa698a34998c8ac",
+    "url": "/static/css/main.6800ca47.chunk.css"
+  },
+  {
+    "revision": "6c4eefa698a34998c8ac",
+    "url": "/static/js/main.6c4eefa6.chunk.js"
+  },
+  {
+    "revision": "e7e0e04a5ea3842eb42e",
+    "url": "/static/js/1.e7e0e04a.chunk.js"
+  },
+  {
+    "revision": "7bde654f2b842dafdeb8",
+    "url": "/static/js/2.7bde654f.chunk.js"
+  },
+  {
+    "revision": "6a4751f849122296e7c6",
+    "url": "/static/js/runtime~main.6a4751f8.js"
+  },
+  {
+    "revision": "5a2fb0ddf58a689c45e613f6a0fe3450",
+    "url": "/static/media/img.5a2fb0dd.jfif"
+  },
+  {
+    "revision": "3371bd2196301669b07c8b99bcd30791",
+    "url": "/static/media/img1.3371bd21.png"
+  },
+  {
+    "revision": "ca0cf099fbcf4dcd0447799dfb069b19",
+    "url": "/static/media/img2.ca0cf099.png"
+  },
+  {
+    "revision": "908c114a8388ca4d1860e9dccabd629a",
+    "url": "/static/media/img3.908c114a.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "e0385b4391520ea38476e9fc74f22509",
+    "url": "/static/media/icons8-facebook-48.e0385b43.svg"
+  },
+  {
+    "revision": "41e7f7a46509e8e0afa3866cb5807c2d",
+    "url": "/index.html"
+  }
+];
