c331d7ddad78d81841f21a2267d6ff5a0b13fee0
Renz Ivan Irag <66311197+Kartoflur@users.noreply.github.com>
2022-06-12 08:20:57 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.df2fdb3992ac630600698dd7d9532eaa.js"
+  "/precache-manifest.6d2cb973e72dc70855bcaa91e60d64a5.js"
 );
 
 workbox.clientsClaim();
